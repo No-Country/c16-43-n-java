d88661f306e33d7f3a03e46f4eba71decb857ad3
Cuch0 <germanosimani@gmail.com>
2024-03-04 17:28:34 -0300

    Cambios en Login

--- front/NoCountryEcommerce/src/app/login/login.component.ts
+++ front/NoCountryEcommerce/src/app/login/login.component.ts
@@ -13,8 +13,10 @@ export class LoginComponent {
     correo: string = ""
     contrasena: string = ""
     alerta: boolean = false
+    alerta2: boolean = false
     administrador: boolean = false
     usuario: boolean = false
+    mostrarCarga: boolean = false
 
     constructor(private inicioComponent: InicioComponent,
                 private http: HttpClient,
@@ -29,6 +31,7 @@ export class LoginComponent {
     }
 
     onSubmit() {
+        this.alerta2 = false
         if (this.correo.trim() === '' || this.contrasena.trim() === '') {
             this.alerta = true
             return;
@@ -41,27 +44,31 @@ export class LoginComponent {
           };
 
           // Aquí puedes enviar datosJSON a tu servidor o realizar otras operaciones con él
-
+        this.mostrarCarga = true;
         this.http.post<any>('https://printopia-backend.onrender.com/api/users/validate', datos).subscribe({
             next: response => {
                 if (response == "ADMIN") {
                     this.administrador = true
                     console.log('Bienvenido administrador', this.administrador);
                     this.inicioSesionService.setInicioSesion(true)
+                    this.mostrarInicio();
                 }
                 if (response == "USER") {
                     this.usuario = true
                     console.log('Bienvenido usuario', this.usuario);
                     this.inicioSesionService.setInicioSesion(true)
+                    this.mostrarInicio();
                 }
+                this.mostrarCarga = false
             },
             error: error => {
                 if (error.status === 401) {
-                    console.log(error.status);
+                    this.alerta2 = true
                   } else {
                     console.error('Error al enviar datos al servidor:', error);
                     // Aquí puedes manejar otros errores de la solicitud HTTP
                   }
+                  this.mostrarCarga = false
                 }
             });
     }
